67752a8639b1bb0011456e436049ac80d3851669
Samuel Alonso <alonso.smontenegro@gmail.com>
2019-04-21 20:29:55 -0300

    fixed font scaling on all displays using JS styling

--- scripts.js
+++ scripts.js
@@ -1,29 +1,36 @@
+function cellFontAdjust (cell) {
+    cell.style.fontSize = `${cell.offsetHeight * 30 / 100}px`; 
+}
+
 function formatCell(cell) {
     cell.style.width = `${cell_width}%`;
     cell.style.height = `${cell_height}%`;
     cell.style.margin = `${cell_margin}%`;
 }
 
-function addCells(N) {
-    for (i=0;i<N**2;i++){
-        let cell = document.createElement("div");
-        cell.classList.add("cell");
-        formatCell(cell);
-        cell.appendChild(document.createElement("p"));
-        cell.childNodes[0].textContent = `${i+1}`
-        board.appendChild(cell);
-        ;
-    }
-}
-
 function resizeCells(){
     board.style.height = `${(board.offsetWidth)}px`;
     board.childNodes.forEach(function (x) {
     formatCell(x);
+    cellFontAdjust(x);
     });
 }
 
-let N = 8;
+function addCells(N) {
+    for (i=0;i<N**2;i++){
+        let cell = document.createElement("div");
+        cell.classList.add("cell");
+
+        cell.appendChild(document.createElement("p"));
+        cell.firstElementChild.textContent = `${i+1}`;
+
+        formatCell(cell);
+        board.appendChild(cell);
+        resizeCells();
+    }
+}
+
+let N = 4;
 
 // Making a truly square grid
 const board = document.querySelector("#board");
